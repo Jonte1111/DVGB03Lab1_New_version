66b78d0ba1105afd078c4dbfcd0f89c7d9ed7185
Jonathan Larsson <jonte1275@gmail.com>
2022-11-18 08:37:45 +0100

    Quick sort works!

--- analyze.c
+++ analyze.c
@@ -1,26 +1,22 @@
 #include "analyze.h"
 #include "algorithm.h"
 #include <stdio.h>
+#include <stdbool.h>
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
 #define BILLION 1E9
 #define MAX_SIZE 16384
 #define MIN_SIZE 512
-#define REPEAT 100 
+#define REPEAT 15 
 
 //
 // Private
 //
+//Function pointers, one for search algo's and one for sorting
 typedef void (*func_pointer_void)(int *a, int n);
 typedef bool (*func_pointer_bool)(const int *a, int n, int v);
-int* generate_same_number(int* arr, int size) {
-	int num = 1;
-	for(int i=0;i<size;i++) 
-		arr[i] = num;
-	return arr;
-}
-//Generates an array of random numbers
+//Generates an array of random numbers, used for most average cases
 int* generate_random(int* arr, int size){
 	srand(time(0));
 	for(int i = 0; i < size; i++) {
@@ -35,16 +31,7 @@ int* generate_sorted(int* arr, int size) {
 	}
 	return arr;
 }
-int* generate_quick_sort(int* arr, int size) {
-	for(int i = 0;i< size/2;i++) 
-		arr[i] = i;	
-	for(int j = size;j>=size/2;j--) 
-		arr[j] = j;	
-	//for(int k = 0;k<size;k++) 
-	//	printf("%d\n", arr[k]);		
-	
-	return arr;
-}
+
 //Generates an array in descending order
 int* generate_unsorted(int* arr, int size) {
 	int count = 0;
@@ -55,21 +42,15 @@ int* generate_unsorted(int* arr, int size) {
 	return arr;
 }
 int* which_arr(int* arr, int size, const case_t c, const algorithm_t a) {
-	/*if(a == binary_search_t || a == linear_search_t) {
-		arr = generate_sorted();
-	}*/
-	//If best case or if a search algo
-	if(c == best_t || a > quick_sort_t) {
+	if(a == binary_search_t || a == linear_search_t)
 		arr = generate_sorted(arr, size);
-	}
-	//If average and sorting algo
-	else if(c == average_t) {
+	else if(a == quick_sort_t && c == best_t)
 		arr = generate_random(arr, size);
-	}
-	//Special case for quick_sort
-	else if(c == worst_t && a == quick_sort_t)
-		arr = generate_quick_sort(arr, size);
-		//arr = generate_same_number(arr, size);
+	else if(c == best_t)
+		arr = generate_sorted(arr, size);
+	//If average and sorting algo
+	else if(c == average_t) 
+		arr = generate_random(arr, size);
 	//Else unsorted list/worst case
 	else
 		arr = generate_unsorted(arr, size);
@@ -81,33 +62,48 @@ void print(int n, double total, const case_t c) {
 		n, total, total/n, total/log2(n), total/(n*log2(n)));
 	}
 	else if(c == average_t) {
-		printf("%d\t%.9fT(s)\t%eT/(N)\t%eT/nlog(n)\t%eT/(n^2)\n",
-		n, total, total/n, total/(n*log2(n)), total/(n*n));
+		printf("%d\t%.9fT(s)\t%eT/(N)\t%eT/nlog(n)\t%eT/(n^2)\t%eT/log(n)\n",
+		n, total, total/n, total/(n*log2(n)), total/(n*n), total/log2(n));
 	}
 	else {
-		printf("%d\t%.9fT(s)\t%eT/nlog(n)\t%eT/n^2\t%eT/n^3\n",
-		n, total, total/(n*log2(n)), total/(n*n), total/(n*n*n));
+		printf("%d\t%.9fT(s)\t%eT/nlog(n)\t%eT/n^2\t%eT/log(n)\n",
+		n, total, total/(n*log2(n)), total/(n*n), total/log2(n));
 	}
 }
 //Measures the time of a function
 //Measures max amounts of times
 //It then returns the average in seconds
-int timer_sort(int repeat, func_pointer_void f, int n, const case_t c, const algorithm_t a){
+int timer_sort(int repeat, func_pointer_void f, const case_t c, const algorithm_t a){
 	struct timespec start, stop;
 	double total = 0;
-	for(int n = 512; n<=16384; n*=2) {
-		int* arrP = (int*)malloc(n * sizeof(int));
-		arrP = which_arr(arrP, n, c, a);
+	int n = MIN_SIZE;
+	int m = MAX_SIZE;
+	//int x;
+	while(n<=m) {
+		//int* arrP = (int*)malloc(n * sizeof(int));
+		//arrP = which_arr(arrP, n, c, a);
+	//	if(a == quick_sort_t)
+	//		x = n / sizeof(arrP[0]);
+	//	else
+	//		x = n;
 		for(int i = 0;i<repeat;i++) {
+			int* arrP = (int*)malloc(n * sizeof(int));
+			arrP = which_arr(arrP, n, c, a);
 			clock_gettime(CLOCK_MONOTONIC, &start);
-			f(arrP, n); 
+			f(arrP, n);
 			clock_gettime(CLOCK_MONOTONIC, &stop);
 			total+=(stop.tv_sec*BILLION+stop.tv_nsec)-
 			(start.tv_sec*BILLION+start.tv_nsec);
+		for(int i = 0;i < n;i++) 
+			printf("%d\n", arrP[i]);
+
+		free(arrP);
 		}
 		total = total/BILLION/repeat;
 		print(n, total, c);
-		free(arrP);
+		//free(arrP);
+		n*=2;
+	
 	}
 	return total;
 }
@@ -120,7 +116,6 @@ int timer_search(const case_t c, const algorithm_t a, int repeat, func_pointer_b
 	while(n <= m ) {
 		int* arrP = (int*)malloc(n * sizeof(int));
 		arrP = generate_sorted(arrP, n);
-		//Scuffed fixa det här
 		if((c == best_t && a == linear_search_t) || (c == worst_t && a == binary_search_t))
 			v = arrP[0];
 		else if(c == worst_t && a == linear_search_t)
@@ -140,12 +135,12 @@ int timer_search(const case_t c, const algorithm_t a, int repeat, func_pointer_b
 		free(arrP);
 		total = total/BILLION/repeat;
 		print(n, total, c);
-		n*=2;
-	}
+			n*=2;
+		}
 	return total;
-}
+	}
 //Generate an array based on the worst case for a given algorithm
-void worst_case(const algorithm_t a, int n, const case_t c){ 
+void worst_case(const algorithm_t a, const case_t c){ 
 	//Create a pointer to a function
 	//and an array
 	int r = REPEAT;
@@ -156,113 +151,78 @@ void worst_case(const algorithm_t a, int n, const case_t c){
 		fvp = bubble_sort;
 		//if algorithm_t > quick_sort_t = search
 		//Worst case for bubblesort is a reversed array
-		printf("Bubble sort worst case:\n");
-		timer_sort(r, fvp, n, c, a);
+		timer_sort(r, fvp ,c, a);
 	}
 	else if(a == insertion_sort_t) {
 		fvp = insertion_sort;
-		printf("Insertion sort worst case:\n");
-		timer_sort(r, fvp, n, c, a);
-	}
-	else if(a == quick_sort_t) {
-		fvp = quick_sort;
-		timer_sort(r, fvp, n, c, a);
-		printf("Quick sort worst case:\n");
-	}
-	else if(a == linear_search_t) {
-		//fbp = linear_search;
-		printf("Linear search worst case:\n");
-		//fbp(arrP, 1, 2);
+		timer_sort(r, fvp,c, a);
 	}
 	else {
-		//fbp = binary_search;
-		printf("Binary search worst case:\n");
-		//fbp(arrP, 1, 2);
+		fvp = quick_sort;
+		timer_sort(r, fvp, c, a);
 	}
 	
 }
-void average_case(const algorithm_t a, int n, const case_t c) {
+void average_case(const algorithm_t a, const case_t c) {
 	int r = REPEAT;
 	//int size = MAX_SIZE;
 	func_pointer_void fvp;
        	if(a == bubble_sort_t)	{
 		fvp = bubble_sort;
 		//Worst case for bubblesort is a reversed array
-		printf("Bubble Sort average case\n");
-		timer_sort(r, fvp, n, c, a);
+		timer_sort(r, fvp, c, a);
 	}
 	else if(a == insertion_sort_t) {
 		fvp = insertion_sort;
 		//Worst case for bubblesort is a reversed array
-		printf("Insertion Sort average case\n");
-		timer_sort(r, fvp, n, c, a);
+		timer_sort(r, fvp, c, a);
 	}
 	else if(a == quick_sort_t) {
 		fvp = quick_sort;
-		timer_sort(r, fvp, n, c, a);
-		printf("Quick Sort average case\n");
+		timer_sort(r, fvp, c, a);
 	}
-	else if(a == linear_search_t) {
-		printf("Linear Search average case\n");
-	}
-	else {
-		printf("Binary Search average case\n");
-		}
 }
-void best_case(const algorithm_t a, int n, const case_t c) {
+void best_case(const algorithm_t a, const case_t c) {
 	int r = REPEAT;
-	//int size = MAX_SIZE;
 	func_pointer_void fvp;
 	if(a == bubble_sort_t)	{
 		fvp = bubble_sort;
-		printf("Bubble Sort best case\n");
-		timer_sort(r, fvp, n, c, a);
+		timer_sort(r, fvp, c, a);
 	}
 	else if(a == insertion_sort_t) {
 		fvp = insertion_sort;
-		printf("Insertion Sort best case\n");
-		timer_sort(r, fvp, n, c, a);
-	}
-	else if(a == quick_sort_t) {
-		fvp = quick_sort;
-		timer_sort(r, fvp, n, c, a);
-		printf("Quick Sort best case\n");
-	}
-	else if(a == linear_search_t) {
-		printf("Linear Search best case\n");
+		timer_sort(r, fvp, c, a);
 	}
 	else {
-		printf("Binary Search best case\n");
+		fvp = quick_sort;
+		timer_sort(r, fvp, c, a);
 	}
 }
-void search_case(const case_t c, const algorithm_t a, int n) {
+void search_case(const case_t c, const algorithm_t a) {
 	int r = REPEAT;
-	//int size = MAX_SIZE;
 	func_pointer_bool fbp;
-	//int* arrP = (int*)malloc(size * sizeof(int));
 	if(a == linear_search_t)
 		fbp = linear_search;
 	else
 		fbp = binary_search;
 	//In all cases the array is sorted
-	printf("search");
 	timer_search(c, a, r, fbp);
 }
 //Determines if case is worst, average or best
-void determine_case(const case_t c, const algorithm_t a, int n) {
+void determine_case(const case_t c, const algorithm_t a) {
 	if(a > quick_sort_t)
-		search_case(c, a, n);
+		search_case(c, a);
 	else if(c == worst_t) 
-		worst_case(a, n, c);
+		worst_case(a, c);
 	else if(c == average_t)
-		average_case(a, n, c);
+		average_case(a, c);
 	else
-		best_case(a, n, c);
+		best_case(a, c);
 }
 //
 // Public
 //
 void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
 {
-	determine_case(c, a, n);
+	determine_case(c, a);
 }
